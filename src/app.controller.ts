7f34ef5dbf42291c19ff478ad4090e89881f9337
ducnnn <ducnn.230288@gmail.com>
2023-09-17 19:27:00 +0700

    add render home

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,80 +1,28 @@
 import { Get, Controller, Render, Req, Post, Res, Param } from '@nestjs/common';
 import { FastifyReply, FastifyRequest } from 'fastify';
-import { DataService, ParameterService } from '@service';
-import { DataDto } from '@dto';
+import { DataService, ParameterService, PostService } from '@service';
+import { DataDto, PostDto } from '@dto';
 import { I18n, I18nContext } from 'nestjs-i18n';
+import { Data } from '@model';
+import dayjs from 'dayjs';
+
 @Controller()
 export class AppController {
   constructor(
     private readonly dataService: DataService,
+    private readonly postService: PostService,
     private readonly parameterService: ParameterService,
   ) {}
 
-  @Get(':lang')
+  @Get('')
   @Render('index')
-  async root(
-    @I18n() i18n: I18nContext,
-    @Param('lang') language: string,
-  ): Promise<{
-    title: string;
-    lang: string;
-    isEnglish: boolean;
-    language: object;
-    parameter: object;
-    partner: DataDto[];
-    mission: DataDto[];
-    services: DataDto[];
-    value: DataDto[];
-    member: DataDto[];
-  }> {
-    const [parameter] = await this.parameterService.findAll({});
-    const returnParameter = {};
-    parameter.forEach((item) => (returnParameter[item.code] = item[language]));
-
-    const dataArray = await this.dataService.findArrayCode(['partner', 'mission', 'services', 'value', 'member']);
+  async root(@I18n() i18n: I18nContext, language: string = 'vn', urlLang = '/en/'): Promise<IHome> {
+    const { data, dataArray } = await this.common(language, ['mission', 'services', 'value', 'member']);
     return {
-      title: 'ARI TECHNOLOGY',
-      lang: language,
-      isEnglish: language == 'en',
+      urlLang,
+      ...data,
       language: {
-        layout: {
-          header: {
-            Location: i18n.t('client.layout.header.Location', { lang: language }),
-            Mail: i18n.t('client.layout.header.Mail', { lang: language }),
-            Call: i18n.t('client.layout.header.Call', { lang: language }),
-            Home: i18n.t('client.layout.header.Home', { lang: language }),
-            About: i18n.t('client.layout.header.About', { lang: language }),
-            AboutTech: i18n.t('client.layout.header.AboutTech', { lang: language }),
-            OurCoreTeam: i18n.t('client.layout.header.OurCoreTeam', { lang: language }),
-            News: i18n.t('client.layout.header.News', { lang: language }),
-            Projects: i18n.t('client.layout.header.Projects', { lang: language }),
-            ContactUs: i18n.t('client.layout.header.ContactUs', { lang: language }),
-            Vietnamese: i18n.t('client.layout.header.Vietnamese', { lang: language }),
-            English: i18n.t('client.layout.header.English', { lang: language }),
-          },
-          footer: {
-            PartnersAndCustomers: i18n.t('client.layout.footer.PartnersAndCustomers', { lang: language }),
-            OurSupportPartner: i18n.t('client.layout.footer.OurSupportPartner', { lang: language }),
-            Contact: i18n.t('client.layout.footer.Contact', { lang: language }),
-            QuestionsOrConcerns: i18n.t('client.layout.footer.QuestionsOrConcerns', { lang: language }),
-            FirstName: i18n.t('client.layout.footer.FirstName', { lang: language }),
-            LastName: i18n.t('client.layout.footer.LastName', { lang: language }),
-            PhoneNumber: i18n.t('client.layout.footer.PhoneNumber', { lang: language }),
-            Email: i18n.t('client.layout.footer.Email', { lang: language }),
-            Message: i18n.t('client.layout.footer.Message', { lang: language }),
-            SubmitNow: i18n.t('client.layout.footer.SubmitNow', { lang: language }),
-            OurServices: i18n.t('client.layout.footer.OurServices', { lang: language }),
-            DigitalTransformation: i18n.t('client.layout.footer.DigitalTransformation', { lang: language }),
-            RDServices: i18n.t('client.layout.footer.RDServices', { lang: language }),
-            OutsourcingServices: i18n.t('client.layout.footer.OutsourcingServices', { lang: language }),
-            ProductDevelopment: i18n.t('client.layout.footer.ProductDevelopment', { lang: language }),
-            UsefulLinks: i18n.t('client.layout.footer.UsefulLinks', { lang: language }),
-            Copyright: i18n.t('client.layout.footer.Copyright', {
-              lang: language,
-              args: { year: new Date().getFullYear() },
-            }),
-          },
-        },
+        ...data.language,
         page: {
           home: {
             EnhanceVietnam: i18n.t('client.page.home.EnhanceVietnam', { lang: language }),
@@ -101,8 +49,6 @@ export class AppController {
           },
         },
       },
-      parameter: returnParameter,
-      partner: dataArray['partner'],
       mission: dataArray['mission'].map((item) => ({
         ...item,
         translation: item.translations?.filter((subItem) => subItem.language === language)[0],
@@ -123,6 +69,151 @@ export class AppController {
     };
   }
 
+  @Get('en')
+  @Render('index')
+  async rootLang(@I18n() i18n: I18nContext): Promise<IHome> {
+    return await this.root(i18n, 'en', '/');
+  }
+
+  @Get('/tin-tuc/')
+  @Render('post/list')
+  async news(
+    @I18n() i18n: I18nContext,
+    language: string = 'vn',
+    type = 'news',
+    url: string = '/tin-tuc/',
+    urlLang = '/en/news/',
+  ): Promise<IListPost> {
+    const { data } = await this.common(language);
+    const postArray = await this.postService.findArrayCode([type]);
+    return {
+      urlLang,
+      ...data,
+      language: {
+        ...data.language,
+        page: {
+          news: {
+            Title: i18n.t(`client.page.${type}.Title`, { lang: language }),
+            Description: i18n.t(`client.page.${type}.Description`, { lang: language }),
+            OtherRelated: i18n.t(`client.page.${type}.OtherRelated`, { lang: language }),
+            MoreUpToDate: i18n.t(`client.page.${type}.MoreUpToDate`, { lang: language }),
+          },
+        },
+      },
+      post: postArray[type].map((item) => {
+        const translation = item.translations?.filter((subItem) => subItem.language === language)[0];
+        return {
+          ...item,
+          SeeMore: i18n.t('client.page.home.SeeMore', { lang: language }),
+          translation: {
+            ...translation,
+            slug: url + translation!.slug,
+          },
+        };
+      }),
+    };
+  }
+  @Get('/en/news/')
+  @Render('post/list')
+  async newsEn(@I18n() i18n: I18nContext): Promise<IListPost> {
+    return await this.news(i18n, 'en', 'news', '/en/news/', '/tin-tuc/');
+  }
+
+  @Get('/du-an/')
+  @Render('post/list')
+  async projects(@I18n() i18n: I18nContext): Promise<IListPost> {
+    return await this.news(i18n, 'vn', 'projects', '/du-an/', '/en/projects/');
+  }
+
+  @Get('/en/projects/')
+  @Render('post/list')
+  async projectsEn(@I18n() i18n: I18nContext): Promise<IListPost> {
+    return await this.news(i18n, 'en', 'projects', '/en/projects/', '/du-an/');
+  }
+
+  @Get('/tin-tuc/:slug/')
+  @Render('post/detail')
+  async newsDetail(
+    @I18n() i18n: I18nContext,
+    @Param('slug') slug: string,
+    @Res({ passthrough: true }) res: FastifyReply,
+    language: string = 'vn',
+    type = 'news',
+    url: string = '/tin-tuc/',
+    urlLang = '/en/news/',
+  ): Promise<IPost | void> {
+    const { data } = await this.common(language);
+    const post = await this.postService.findSlug(slug);
+    if (!post) res.redirect(404, '/404');
+    else {
+      const postArray = await this.postService.findArrayCode([type]);
+      const translation = post.translations?.filter((subItem) => subItem.language === language)[0];
+      return {
+        urlLang: urlLang + post.translations?.filter((subItem) => subItem.language !== language)[0].slug,
+        ...data,
+        language: {
+          ...data.language,
+          page: {
+            news: {
+              Title: i18n.t(`client.page.${type}.Title`, { lang: language }),
+              Description: i18n.t(`client.page.${type}.Description`, { lang: language }),
+              OtherRelated: i18n.t(`client.page.${type}.OtherRelated`, { lang: language }),
+              MoreUpToDate: i18n.t(`client.page.${type}.MoreUpToDate`, { lang: language }),
+            },
+          },
+        },
+        post: postArray[type].map((item) => {
+          const translation = item.translations?.filter((subItem) => subItem.language === language)[0];
+          return {
+            ...item,
+            createdAt: dayjs(item.createdAt).format('DD-MM-YYYY'),
+            SeeMore: i18n.t('client.page.home.SeeMore', { lang: language }),
+            translation: {
+              ...translation,
+              slug: url + translation!.slug,
+            },
+          };
+        }),
+        detail: {
+          ...post,
+          translation: {
+            ...translation,
+
+            slug: url + translation!.slug,
+            content: this.renderEditor(translation!.content!.blocks),
+          },
+        },
+      };
+    }
+  }
+  @Get('/en/news/:slug/')
+  @Render('post/detail')
+  async newsDetailEn(
+    @I18n() i18n: I18nContext,
+    @Param('slug') slug: string,
+    @Res({ passthrough: true }) res: FastifyReply,
+  ): Promise<IPost | void> {
+    return await this.newsDetail(i18n, slug, res, 'en', 'news', '/en/news/', '/tin-tuc/');
+  }
+
+  @Get('/du-an/:slug/')
+  @Render('post/detail')
+  async projectsDetail(
+    @I18n() i18n: I18nContext,
+    @Param('slug') slug: string,
+    @Res({ passthrough: true }) res: FastifyReply,
+  ): Promise<IPost | void> {
+    return await this.newsDetail(i18n, slug, res, 'vn', 'projects', '/du-an/', '/en/projects/');
+  }
+  @Get('/en/projects/:slug/')
+  @Render('post/detail')
+  async projectsDetailEn(
+    @I18n() i18n: I18nContext,
+    @Param('slug') slug: string,
+    @Res({ passthrough: true }) res: FastifyReply,
+  ): Promise<IPost | void> {
+    return await this.newsDetail(i18n, slug, res, 'en', 'projects', '/en/projects/', '/du-an/');
+  }
   @Post('/')
   login(@Req() req: FastifyRequest, @Res({ passthrough: true }) res: FastifyReply) {
     req.session.set('data', 'req.body');
@@ -133,4 +224,125 @@ export class AppController {
   @Get('/administrator')
   @Render('administrator')
   administrator() {}
+
+  async common(
+    language: string,
+    arrayCode: string[] = [],
+  ): Promise<{
+    data: ICommon;
+    dataArray: { [p: string]: Data[] };
+  }> {
+    const i18n = I18nContext.current()!;
+    const [parameter] = await this.parameterService.findAll({});
+    const returnParameter = {};
+    parameter.forEach((item) => (returnParameter[item.code] = item[language]));
+
+    const dataArray = await this.dataService.findArrayCode(['partner', ...arrayCode]);
+    return {
+      data: {
+        title: 'ARI TECHNOLOGY',
+        lang: language,
+        isEnglish: language == 'en',
+        language: {
+          layout: {
+            header: {
+              Location: i18n.t('client.layout.header.Location', { lang: language }),
+              Mail: i18n.t('client.layout.header.Mail', { lang: language }),
+              Call: i18n.t('client.layout.header.Call', { lang: language }),
+              Home: i18n.t('client.layout.header.Home', { lang: language }),
+              About: i18n.t('client.layout.header.About', { lang: language }),
+              AboutTech: i18n.t('client.layout.header.AboutTech', { lang: language }),
+              OurCoreTeam: i18n.t('client.layout.header.OurCoreTeam', { lang: language }),
+              News: i18n.t('client.layout.header.News', { lang: language }),
+              Projects: i18n.t('client.layout.header.Projects', { lang: language }),
+              ContactUs: i18n.t('client.layout.header.ContactUs', { lang: language }),
+              Vietnamese: i18n.t('client.layout.header.Vietnamese', { lang: language }),
+              English: i18n.t('client.layout.header.English', { lang: language }),
+            },
+            footer: {
+              PartnersAndCustomers: i18n.t('client.layout.footer.PartnersAndCustomers', { lang: language }),
+              OurSupportPartner: i18n.t('client.layout.footer.OurSupportPartner', { lang: language }),
+              Contact: i18n.t('client.layout.footer.Contact', { lang: language }),
+              QuestionsOrConcerns: i18n.t('client.layout.footer.QuestionsOrConcerns', { lang: language }),
+              FirstName: i18n.t('client.layout.footer.FirstName', { lang: language }),
+              LastName: i18n.t('client.layout.footer.LastName', { lang: language }),
+              PhoneNumber: i18n.t('client.layout.footer.PhoneNumber', { lang: language }),
+              Email: i18n.t('client.layout.footer.Email', { lang: language }),
+              Message: i18n.t('client.layout.footer.Message', { lang: language }),
+              SubmitNow: i18n.t('client.layout.footer.SubmitNow', { lang: language }),
+              OurServices: i18n.t('client.layout.footer.OurServices', { lang: language }),
+              DigitalTransformation: i18n.t('client.layout.footer.DigitalTransformation', { lang: language }),
+              RDServices: i18n.t('client.layout.footer.RDServices', { lang: language }),
+              OutsourcingServices: i18n.t('client.layout.footer.OutsourcingServices', { lang: language }),
+              ProductDevelopment: i18n.t('client.layout.footer.ProductDevelopment', { lang: language }),
+              UsefulLinks: i18n.t('client.layout.footer.UsefulLinks', { lang: language }),
+              Copyright: i18n.t('client.layout.footer.Copyright', {
+                lang: language,
+                args: { year: new Date().getFullYear() },
+              }),
+            },
+          },
+        },
+        parameter: returnParameter,
+        partner: dataArray['partner'],
+      },
+      dataArray,
+    };
+  }
+
+  renderEditor(content: IEditor[]) {
+    return content.map(({ type, data }) => {
+      switch (type) {
+        case 'header':
+          return `<h${data.level}>${data.text}</h${data.level}>`;
+        case 'image':
+          return ` <a class="image glightbox" href=${data.file?.url} data-description=${
+            data.caption
+          }><img class="lazy" alt="${data.caption}" data-src="${data.file?.url}"/>${
+            data.caption ? '<span class="caption">' + data.caption + '</span>' : ''
+          }</a>`;
+        default:
+          return `<p>${data.text}</p>`;
+      }
+    });
+  }
+}
+interface ICommon {
+  title: string;
+  lang: string;
+  isEnglish: boolean;
+  language: object;
+  parameter: object;
+  partner: DataDto[];
+}
+interface IHome extends ICommon {
+  urlLang: string;
+  mission: DataDto[];
+  services: DataDto[];
+  value: DataDto[];
+  member: DataDto[];
+}
+interface IListPost extends ICommon {
+  urlLang: string;
+  post: PostDto[];
+}
+interface IPost extends ICommon {
+  urlLang: string;
+  post: PostDto[];
+  detail: object;
+}
+interface IEditor {
+  id: string;
+  data: {
+    text: string;
+    level?: string;
+    file?: {
+      url?: string;
+    };
+    caption?: string;
+    stretched?: boolean;
+    withBorder?: boolean;
+    withBackground?: boolean;
+  };
+  type: string;
 }
