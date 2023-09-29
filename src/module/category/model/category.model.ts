a83bae8ef4929c9fb80fdd9932f09fd6c0088e0a
danhthien1008 <tkgiongmk010@gmail.com>
2023-09-29 08:12:32 +0700

    full model product-category

--- src/module/category/model/category.model.ts
+++ src/module/category/model/category.model.ts
@@ -1,23 +1,37 @@
-import { Entity, OneToMany } from 'typeorm';
-import { IsString, IsNumber } from 'class-validator';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { IsString, IsNumber, IsArray } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { faker } from '@faker-js/faker';
+import { Expose } from 'class-transformer';
 
 import { Base } from '@shared';
 import { Product } from '@model';
 
 @Entity()
 export class Category extends Base {
+  @Column()
+  @ApiProperty({ example: faker.person.fullName(), description: '' })
   @IsString()
+  @Expose()
   name: string;
 
+  @Column()
+  @ApiProperty({ example: faker.lorem.paragraph(), description: '' })
   @IsString()
+  @Expose()
   description: string;
 
+  @Column()
+  @ApiProperty({ example: faker.lorem.slug(), description: '' })
   @IsString()
+  @Expose()
   slug: string;
 
+  @Column()
   @IsNumber()
   parentId: number;
 
-  @OneToMany(() => Product, (product) => product.category)
-  products?: Product[];
+  @OneToMany(() => Product, (product) => product.category, { eager: true })
+  @IsArray()
+  public products?: Product[];
 }
--- src/module/product/model/product.model.ts
+++ src/module/product/model/product.model.ts
@@ -1,44 +1,90 @@
-import { Entity, ManyToOne } from 'typeorm';
-import { IsString, IsNumber } from 'class-validator';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { IsString, IsNumber, IsUUID, IsOptional, IsPositive } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { faker } from '@faker-js/faker';
+import { Exclude, Expose } from 'class-transformer';
 
-import { Base } from '@shared';
+import { Base, MaxGroup } from '@shared';
 import { Category, Store } from '@model';
-import { Type } from 'class-transformer';
 
 @Entity()
 export class Product extends Base {
+  @Column()
   @IsString()
+  @ApiProperty({ example: faker.person.fullName(), description: '' })
+  @Expose()
   name: string;
 
+  @Column()
   @IsString()
+  @ApiProperty({ example: faker.lorem.paragraph(), description: '' })
+  @Expose()
   description: string;
 
+  @Column()
   @IsNumber()
+  @IsPositive()
+  @ApiProperty({ example: faker.number.int({ max: 100 }) })
+  @Expose()
   quantity: number;
 
+  @Column()
   @IsNumber()
+  @IsPositive()
+  @ApiProperty({ example: faker.number.int() })
+  @Expose()
   price: number;
 
+  @Column()
   @IsString()
+  @Expose()
   images: string;
 
+  @Column({ default: 0 })
+  @ApiProperty({ example: 0, description: '' })
   @IsNumber()
+  @Exclude()
   status: number;
 
+  @Column()
+  @ApiProperty({ example: faker.lorem.slug(), description: '' })
   @IsString()
+  @Expose()
   slug: string;
 
+  @Column()
+  @ApiProperty({ example: faker.number.float() })
   @IsString()
+  @IsPositive()
+  @Expose()
   mass: string;
 
+  @Column()
   @IsNumber()
+  @ApiProperty({ example: faker.number.int(100) })
+  @IsPositive()
+  @IsOptional()
+  @Expose()
   disCount: number;
 
-  @ManyToOne(() => Store, (store) => store.products)
-  @Type(() => Store)
-  store: Store;
+  @Column()
+  @Expose({ groups: [MaxGroup] })
+  @IsUUID()
+  categoryId?: string;
 
-  @ManyToOne(() => Category, (category) => category.products)
-  @Type(() => Category)
-  category: Category;
+  @Column()
+  @Expose({ groups: [MaxGroup] })
+  @IsUUID()
+  @IsOptional()
+  storeId?: string;
+
+  @ManyToOne(() => Store, (store) => store.products)
+  @Expose({ groups: [MaxGroup] })
+  public store: Store;
+
+  @ManyToOne(() => Category, (category) => category.products, {
+    eager: false,
+  })
+  @Expose({ groups: [MaxGroup] })
+  public category: Category;
 }
