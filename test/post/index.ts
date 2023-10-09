5f19795c7c4bfdc4364ffc735a97ce1c3e4f6f7c
danhthien1008 <tkgiongmk010@gmail.com>
2023-10-09 08:28:23 +0700

    change test product

---
+++ test/post/index.ts
@@ -0,0 +1,128 @@
+import request from 'supertest';
+import { faker } from '@faker-js/faker';
+import { HttpStatus } from '@nestjs/common';
+
+import { UpdatePostTypeRequestDto, UpdatePostRequestDto, CreatePostRequestDto, CreatePostTypeRequestDto } from '@dto';
+import { Post, PostType } from '@model';
+import { DataService, PostTypeService } from '@service';
+
+import { BaseTest } from '../base';
+
+export const testCase = (type?: string, permissions: string[] = []) => {
+  beforeAll(() => BaseTest.initBeforeAll(type, permissions));
+  afterAll(BaseTest.initAfterAll);
+
+  const dataType: CreatePostTypeRequestDto = {
+    name: faker.person.jobType(),
+    code: faker.finance.bic(),
+  };
+
+  let resultType: PostType = {
+    id: faker.string.uuid(),
+    name: faker.person.jobType(),
+    code: faker.finance.bic(),
+    isPrimary: false,
+  };
+
+  const data: CreatePostRequestDto = {
+    type: dataType.code,
+    thumbnailUrl: faker.image.url(),
+    translations: [
+      {
+        language: 'vn',
+        name: faker.person.jobType(),
+        description: faker.lorem.paragraph(),
+        slug: faker.lorem.slug(),
+      },
+      {
+        language: 'en',
+        name: faker.person.jobType(),
+        description: faker.lorem.paragraph(),
+        slug: faker.lorem.slug(),
+      },
+    ],
+  };
+
+  const dataUpdate: UpdatePostRequestDto = {
+    type: dataType.code,
+    thumbnailUrl: faker.image.url(),
+    translations: [
+      {
+        language: 'vn',
+        name: faker.person.jobType(),
+        description: faker.lorem.paragraph(),
+        slug: faker.lorem.slug(),
+      },
+      {
+        language: 'en',
+        name: faker.person.jobType(),
+        description: faker.lorem.paragraph(),
+        slug: faker.lorem.slug(),
+      },
+    ],
+  };
+
+  const result: Post = {
+    id: faker.string.uuid(),
+    type: resultType.code,
+    thumbnailUrl: faker.image.url(),
+  };
+
+  it('Create [POST /api/post-type/add]', async () => {
+    const { body } = await request(BaseTest.server)
+      .post('/api/post-type/add')
+      .set('Authorization', 'Bearer ' + BaseTest.token)
+      .send(dataType as CreatePostTypeRequestDto)
+      .expect(type ? HttpStatus.CREATED : HttpStatus.FORBIDDEN);
+
+    if (type) {
+      expect(body.data).toEqual(jasmine.objectContaining(dataType));
+      resultType = body.data;
+    }
+  });
+
+  it('Get all [GET /api/post-type/list]', async () => {
+    const { body } = await request(BaseTest.server)
+      .get('/api/post-type/list')
+      .set('Authorization', 'Bearer ' + BaseTest.token)
+      .expect(type ? HttpStatus.OK : HttpStatus.FORBIDDEN);
+    if (type) {
+      expect(body.data[0]).toEqual(jasmine.objectContaining(dataType));
+    }
+  });
+
+  it('Get one [GET /api/post-type/:id]', async () => {
+    if (!type) {
+      resultType = await BaseTest.moduleFixture.get(PostTypeService).create(dataType);
+    }
+    const { body } = await request(BaseTest.server)
+      .get('/api/post-type/' + resultType.id)
+      .set('Authorization', 'Bearer ' + BaseTest.token)
+      .expect(HttpStatus.OK);
+    if (type) {
+      expect(body.data).toEqual(jasmine.objectContaining(dataType));
+    }
+  });
+
+  it('Update one [PUT /api/post-type/:id]', async () => {
+    const { body } = await request(BaseTest.server)
+      .put('/api/post-type/' + resultType.id)
+      .set('Authorization', 'Bearer ' + BaseTest.token)
+      .send(dataType)
+      .expect(type ? HttpStatus.OK : HttpStatus.FORBIDDEN);
+
+    if (type) {
+      expect(body.data).toEqual(jasmine.objectContaining(dataType));
+    }
+  });
+
+  it('Delete one [DELETE /api/post-type/:id]', async () => {
+    const { body } = await request(BaseTest.server)
+      .delete('/api/post-type/' + resultType.id)
+      .set('Authorization', 'Bearer ' + BaseTest.token)
+      .expect(type ? HttpStatus.OK : HttpStatus.FORBIDDEN);
+    if (type) {
+      expect(body.data).toEqual(jasmine.objectContaining(dataType));
+    }
+  });
+};
