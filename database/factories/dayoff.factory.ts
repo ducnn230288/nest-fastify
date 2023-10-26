6a2860c464233406be443340d003eb45ab58177e
lehuynhanhtai <lehuynhanhtai@gmail.com>
2023-10-26 15:14:41 +0700

    update testcase dayoff

--- database/factories/dayoff.factory.ts
+++ database/factories/dayoff.factory.ts
@@ -3,21 +3,13 @@ import { DayOff } from '@model';
 
 export default setSeederFactory(DayOff, (faker) => {
   const data = new DayOff();
-  data.code = '230715000001';
+  data.code = faker.string.numeric(12);
   data.type = faker.number.int({ min: 1, max: 3 });
-  // data.status = faker.number.int({ min: -1, max: 1 });
   data.reason = faker.lorem.paragraph();
   data.time = faker.number.int({ min: 0, max: 2 });
-  // data.timeNumber = faker.number.int({ min: 0.5, max: 1 });
-  // data.timeNumber = 1;
   data.image = faker.image.url();
   data.dateLeaveStart = faker.date.soon({ days: 1 });
   data.dateLeaveEnd = faker.date.soon({ days: 10 });
-  // data.approvedAt = faker.date.soon({ days: 10 });
-  //   data.approvedById
-  // data.reasonReject = faker.lorem.paragraph();
-  // staffId
-  // data.managerId
 
   return data;
 });
--- test/member/dayoff/index.ts
+++ test/member/dayoff/index.ts
@@ -6,29 +6,52 @@ import { useSeederFactoryManager } from 'typeorm-extension';
 import '@factories';
 import { BaseTest } from '@test';
 import { CreateDayoffRequestDto, CreateUserRequestDto } from '@dto';
-import { DayOff, User } from '@model';
-import { UserService } from '@service';
+import { DayOff, User, UserRole } from '@model';
+import { P_DAYOFF_CREATE, UserRoleService, UserService } from '@service';
 import { Example } from '@shared';
 
 export const testCase = (type?: string, permissions: string[] = []): void => {
   beforeAll(() => BaseTest.initBeforeAll(type, permissions));
   const factoryManager = useSeederFactoryManager();
   let dataDayoff: CreateDayoffRequestDto;
-  let resultDayoff;
+  let resultDayoff: DayOff | null;
 
   it('Create [POST /api/dayoff]', async () => {
     dataDayoff = await factoryManager.get(DayOff).make();
-    console.log(dataDayoff);
 
-    // const {body} = await request(BaseTest.server)
-    // .post('/api/dayoff')
-    // .set('Authorization', 'Bearer ' + BaseTest.token)
-    // .send(dataDayoff)
-    // .expect(type ? HttpStatus.CREATED : HttpStatus.FORBIDDEN);
-    // console.log(body);
+    const { body } = await request(BaseTest.server)
+      .post('/api/dayoff')
+      .set('Authorization', 'Bearer ' + BaseTest.token)
+      .send(dataDayoff)
+      .expect(type ? HttpStatus.CREATED : HttpStatus.FORBIDDEN);
+    if (type) {
+      const { dateLeaveEnd, dateLeaveStart, code, ...test } = dataDayoff;
+      expect(body.data).toEqual(jasmine.objectContaining(test));
+      resultDayoff = body.data;
+    }
   });
-  // it('Get list [GET /api/dayoff]', async () => {});
-  // it('Get detail [GET /api/dayoff/{id}]', async () => {});
+
+  it('Get list [GET /api/dayoff]', async () => {
+    const { body } = await request(BaseTest.server)
+      .get('/api/dayoff')
+      .set('Authorization', 'Bearer ' + BaseTest.token)
+      .expect(type ? HttpStatus.OK : HttpStatus.FORBIDDEN);
+    if (type) {
+      const { dateLeaveEnd, dateLeaveStart, code, ...test } = dataDayoff;
+      expect(body.data[0]).toEqual(jasmine.objectContaining(test));
+    }
+  });
+  // it('Get detail [GET /api/dayoff/{id}]', async () => {
+  //   const dataRole = await factoryManager.get(UserRole).make({
+  //     permissions: [P_DAYOFF_CREATE],
+  //   });
+  //   const resultRole = await BaseTest.moduleFixture?.get(UserRoleService).create(dataRole);
+  //   const dataUser = {
+  //     ...(await factoryManager.get(User).make({ roleCode: resultRole?.code })),
+  //     retypedPassword: Example.password,
+  //   };
+  //   const resultUser = await BaseTest.moduleFixture?.get(UserService).create(dataUser);
+  // });
   // it('Update [PUT /api/dayoff/{id}]', async () => {});
   // it('Update data status [PUT /api/dayoff/{id}/status]', async () => {});
   // it('Delete [DELETE /api/dayoff/{id}]', async () => {});
