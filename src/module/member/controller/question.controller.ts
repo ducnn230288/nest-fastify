0ec2949c17c6cbd613d6727fe094e1bbe527ae42
danhthien1008 <tkgiongmk010@gmail.com>
2023-11-13 10:30:10 +0700

    merge code

--- src/module/member/controller/question.controller.ts
+++ src/module/member/controller/question.controller.ts
@@ -1,101 +1,20 @@
-<<<<<<< HEAD
-import { Body, Delete, Get, Param, Post, Put, Query } from "@nestjs/common";
-import { Auth, Headers, MaxGroup, PaginationQueryDto, SerializerBody } from "@shared";
-import { I18n, I18nContext } from "nestjs-i18n";
-import { CreateQuestionRequestDto, ListQuestionResponseDto, QuestionResponseDto, UpdateQuestionRequestDto } from "@dto";
-import { P_QUESTION_CREATE, P_QUESTION_DELETE, P_QUESTION_DETAIL, P_QUESTION_LISTED, P_QUESTION_UPDATE, QuestionService } from "@service";
-=======
-import { Body, Post } from '@nestjs/common';
-import { Auth, Headers, MaxGroup, SerializerBody } from '@shared';
+import { Body, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Auth, Headers, MaxGroup, PaginationQueryDto, SerializerBody } from '@shared';
 import { I18n, I18nContext } from 'nestjs-i18n';
-import { CreateQuestionRequestDto } from '@dto';
-import { P_QUESTION_CREATE, QuestionService } from '@service';
->>>>>>> 0e8ab6fd370cf79da91d0dec3aca31e7f23e7d43
+import { CreateQuestionRequestDto, ListQuestionResponseDto, QuestionResponseDto, UpdateQuestionRequestDto } from '@dto';
+import {
+  P_QUESTION_CREATE,
+  P_QUESTION_DELETE,
+  P_QUESTION_DETAIL,
+  P_QUESTION_LISTED,
+  P_QUESTION_UPDATE,
+  QuestionService,
+} from '@service';
 
 @Headers('question')
 export class QuestionController {
   constructor(private readonly service: QuestionService) {}
 
-<<<<<<< HEAD
-    constructor(
-        private readonly service: QuestionService
-    ) { }
-
-    @Auth({
-        summary: 'Create question',
-        permission: P_QUESTION_CREATE,
-        serializeOptions: { groups: [MaxGroup] },
-    })
-    @Post('')
-    async create(
-        @I18n() i18n: I18nContext,
-        @Body(new SerializerBody()) body: CreateQuestionRequestDto,
-    ): Promise<any> {
-        return {
-            message: i18n.t('common.Create Success'),
-            data: await this.service.create(body),
-        }
-    }
-
-
-    @Auth({
-        summary: 'Get List Team',
-        permission: P_QUESTION_LISTED,
-    })
-    @Get('')
-    async findAll(@I18n() i18n: I18nContext, @Query() paginationQuery: PaginationQueryDto): Promise<ListQuestionResponseDto> {
-        const [result, total] = await this.service.findAll(paginationQuery);
-        return {
-            message: i18n.t('common.Get List success'),
-            count: total,
-            data: result,
-        };
-    }
-
-    @Auth({
-        summary: 'Get List Team',
-        permission: P_QUESTION_DETAIL,
-    })
-    @Get(':id')
-    async findOne(@I18n() i18n: I18nContext, @Param('id') id: string): Promise<QuestionResponseDto> {
-        return {
-            message: i18n.t('common.Get List success'),
-            data: await this.service.findOne(id, []),
-        };
-    }
-
-    @Auth({
-        summary: 'Update question',
-        permission: P_QUESTION_UPDATE,
-        serializeOptions: { groups: [MaxGroup] },
-    })
-    @Put(':id')
-    async update(
-        @I18n() i18n: I18nContext,
-        @Param('id') id: string,
-        @Body(new SerializerBody()) body: UpdateQuestionRequestDto,
-    ): Promise<QuestionResponseDto | any> {
-        return {
-            message: i18n.t('common.Update Success'),
-            data: await this.service.update(id, body),
-        };
-    }
-
-    @Auth({
-        summary: 'Delete question',
-        permission: P_QUESTION_DELETE,
-        serializeOptions: { groups: [MaxGroup] },
-    })
-    @Delete(':id')
-    async remove(@I18n() i18n: I18nContext, @Param('id') id: string): Promise<any> {
-        return {
-            message: i18n.t('common.Delete Success'),
-            data: await this.service.remove(id),
-        };
-    }
-
-}
-=======
   @Auth({
     summary: 'Create question',
     permission: P_QUESTION_CREATE,
@@ -108,5 +27,63 @@ export class QuestionController {
       data: await this.service.create(body),
     };
   }
+
+  @Auth({
+    summary: 'Get List Team',
+    permission: P_QUESTION_LISTED,
+  })
+  @Get('')
+  async findAll(
+    @I18n() i18n: I18nContext,
+    @Query() paginationQuery: PaginationQueryDto,
+  ): Promise<ListQuestionResponseDto> {
+    const [result, total] = await this.service.findAll(paginationQuery);
+    return {
+      message: i18n.t('common.Get List success'),
+      count: total,
+      data: result,
+    };
+  }
+
+  @Auth({
+    summary: 'Get List Team',
+    permission: P_QUESTION_DETAIL,
+  })
+  @Get(':id')
+  async findOne(@I18n() i18n: I18nContext, @Param('id') id: string): Promise<QuestionResponseDto> {
+    return {
+      message: i18n.t('common.Get List success'),
+      data: await this.service.findOne(id, []),
+    };
+  }
+
+  @Auth({
+    summary: 'Update question',
+    permission: P_QUESTION_UPDATE,
+    serializeOptions: { groups: [MaxGroup] },
+  })
+  @Put(':id')
+  async update(
+    @I18n() i18n: I18nContext,
+    @Param('id') id: string,
+    @Body(new SerializerBody()) body: UpdateQuestionRequestDto,
+  ): Promise<QuestionResponseDto | any> {
+    return {
+      message: i18n.t('common.Update Success'),
+      data: await this.service.update(id, body),
+    };
+  }
+
+  @Auth({
+    summary: 'Delete question',
+    permission: P_QUESTION_DELETE,
+    serializeOptions: { groups: [MaxGroup] },
+  })
+  @Delete(':id')
+  async remove(@I18n() i18n: I18nContext, @Param('id') id: string): Promise<any> {
+    return {
+      message: i18n.t('common.Delete Success'),
+      data: await this.service.remove(id),
+    };
+  }
 }
->>>>>>> 0e8ab6fd370cf79da91d0dec3aca31e7f23e7d43
--- src/module/member/dto/question.dto.ts
+++ src/module/member/dto/question.dto.ts
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import { PartialType, PickType } from "@nestjs/swagger";
-import { DefaultResponsesDto, PaginationResponsesDto } from "@shared";
-import { Question, QuestionTest } from "@model";
-=======
 import { PartialType, PickType } from '@nestjs/swagger';
-import { DefaultResponsesDto } from '@shared';
+import { DefaultResponsesDto, PaginationResponsesDto } from '@shared';
 import { Question, QuestionTest } from '@model';
->>>>>>> 0e8ab6fd370cf79da91d0dec3aca31e7f23e7d43
 
 export class QuestionRequestDto extends PartialType(DefaultResponsesDto) {}
 
@@ -18,25 +12,17 @@ export class CreateQuestionRequestDto extends PickType(Question, [
   'typeCode',
   'image',
 ]) {}
-
-<<<<<<< HEAD
-export class CreateQuestionRequestDto extends PickType(Question, ['question', 'options', 'correct', 'level', 'typeCode', 'image']) { }
-export class UpdateQuestionRequestDto extends PartialType(CreateQuestionRequestDto) { }
-
-export class CreateQuestionTestRequestDto extends PickType(QuestionTest, ['answer']) {
-}
+export class UpdateQuestionRequestDto extends PartialType(CreateQuestionRequestDto) {}
 
 export class ListQuestionResponseDto extends PartialType(PaginationResponsesDto) {
-    readonly data: Question[];
+  readonly data: Question[];
 }
 
 export class QuestionResponseDto extends PartialType(DefaultResponsesDto) {
-    readonly data: Question | null;
+  readonly data: Question | null;
 }
 
 export class ListQuestionTestResponseDto extends PartialType(PaginationResponsesDto) {
-    readonly data: QuestionTest[];
+  readonly data: QuestionTest[];
 }
-=======
 export class CreateQuestionTestRequestDto extends PickType(QuestionTest, ['answer']) {}
->>>>>>> 0e8ab6fd370cf79da91d0dec3aca31e7f23e7d43
