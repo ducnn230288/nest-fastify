073f3fb8a84d4c61298400f61765eb9c45ed1ff6
ducnn230288 <ducnn230288@gmail.com>
2024-04-14 12:38:13 +0700

    "Remove unnecessary TypeORM module imports in Building module."

--- src/module/building/building.module.ts
+++ src/module/building/building.module.ts
@@ -1,37 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { BuildingService } from '@service';
 import { BuildingRepository, RoomRepository } from '@repository';
-import { Building, Room } from '@model';
-import { AppUser } from './model/app-user.entity';
-import { BuildingAddress } from './model/building-address.entity';
-import { BuildingContent } from './model/building-content.entity';
-import { BuildingMedia } from './model/building-media.entity';
-import { BuildingUtility } from './model/building-utility.entity';
-import { RoomContent } from './model/room-content.entity';
-import { RoomCost } from './model/room-cost.entity';
-import { RoomMedia } from './model/room-media.entity';
-import { RoomSchedule } from './model/room-schedule.entity';
-import { RoomSupplies } from './model/room-supplies.entity';
-import { RoomUtility } from './model/room-utility.entity';
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      Building,
-      Room,
-      AppUser,
-      BuildingAddress,
-      BuildingContent,
-      BuildingMedia,
-      BuildingUtility,
-      RoomContent,
-      RoomCost,
-      RoomMedia,
-      RoomSchedule,
-      RoomSupplies,
-      RoomUtility,
-    ]),
-  ],
+  imports: [],
   controllers: [],
   providers: [BuildingRepository, RoomRepository, BuildingService],
   exports: [BuildingService],
