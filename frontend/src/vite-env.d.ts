732df0c403ac6ca09145d60cae1f29c571e8b9ee
ducnn230288 <ducnn230288@gmail.com>
2024-04-22 21:20:09 +0700

    fix bug

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 declare module '*.svg' {
+  import * as echarts from 'echarts';
+  import * as GLightbox from 'glightbox';
   import * as React from 'react';
   const ReactComponent: React.FunctionComponent<React.ComponentProps<'svg'> & { title?: string }>;
 
@@ -18,7 +20,7 @@ interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
 
-declare const GLightbox: any;
-declare const echarts: any;
+declare const GLightbox: GLightbox;
+declare const echarts: echarts;
 declare const Swal: any;
 declare const SUNEDITOR: any;
